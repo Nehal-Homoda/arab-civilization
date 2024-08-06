7b6033c85a7c4500d8775629e09ac3f8d1108696
nehal <nehalhomoda00@gmail.com>
2024-08-06 18:44:11 +0300

    first-commit

---
+++ src/components/MainPage.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="main-page">
+        <div class="header">
+            <!-- <div class="btn-wrapper"> -->
+            <button class="rules-btn">قواعد المكافات</button>
+            <!-- </div> -->
+
+            <Timer />
+        </div>
+        <div class="btn-wrapper">
+            <button @click="changeType('receiver')" :class="{ 'active': selectedType == 'receiver' }" class="">قائمه
+                الاستلام</button>
+            <button @click="changeType('senders')" :class="{ 'active': selectedType == 'senders' }">قائمه الارسال</button>
+
+        </div>
+    </div>
+</template>
+
+
+<script>
+import Timer from '@/components/Timer.vue'
+export default {
+    components: {
+        Timer
+    },
+    data: function () {
+
+        return {
+            selectedType: ''
+        }
+    },
+    methods: {
+        changeType(type) {
+            this.selectedType = type
+
+        }
+    }
+}
+
+
+</script>
+
+
+
+<style lang="scss" scoped>
+.btn-wrapper {
+    width: 100%;
+    height: 20vw;
+    display: flex;
+    justify-content: space-between;
+    margin: -60px auto;
+    align-items: center;
+
+
+
+    button {
+        width: 45%;
+        height: 100%;
+        background: url('@/assets/imgs/uncheck-btn.png') center no-repeat;
+        background-size: contain;
+        border: none;
+        outline: none;
+        padding-top: 20px;
+        color: rgb(250, 217, 150);
+        &.active{
+            background: url('@/assets/imgs/check-btn.png') center no-repeat;
+            background-size: contain;
+        }
+
+    }
+
+}
+
+.header {
+    width: 100%;
+    height: 144vw;
+    background: url('@/assets/imgs/header-bg.png') no-repeat;
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 15px;
+    align-items: center;
+    padding-top: 370px;
+
+    .rules-btn {
+        width: 60vw;
+        height: 14vw;
+        background: url('@/assets/imgs/rules-bg.png') no-repeat;
+        background-size: contain;
+        border: none;
+        outline: none;
+        color: rgb(250, 217, 150);
+        font-weight: bold;
+    }
+
+}
+</style>
---
+++ src/components/Timer.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="timer-wrapper">
+        
+        <div class="date-item">
+            <span>ثانيه</span>
+            <div class="date-bg" >
+           <span></span>
+          </div>
+        </div>
+
+        <div class="date-item">
+            <span>دقيقه</span>
+            <div class="date-bg" >
+           <span></span>
+          </div>
+
+          <div class="date-item">
+              <span>ساعه</span>
+              <div class="date-bg" >
+             <span></span>
+            </div>
+  
+          </div>
+        </div>
+        <div class="date-item">
+            <span>يوم</span>
+          <div class="date-bg" >
+           <span></span>
+          </div>
+
+        </div>
+        <span>وقت الحدث</span>
+
+    </div>
+</template>
+
+
+<script>
+
+
+</script>
+
+
+<style lang="scss" scoped>
+.timer-wrapper{
+    display: flex;
+    color: white;
+    align-items: center;
+     gap: 8px;
+    
+   
+    .date-item{
+        display: flex;
+        align-items: center;
+        gap: 5px;
+       
+        
+        
+        .date-bg{
+            width: 7vw;
+            height: 7vw;
+            background: url('@/assets/imgs/timer-bg.png')  center no-repeat;
+            background-size: contain; 
+
+        }
+    }
+}
+
+</style>
