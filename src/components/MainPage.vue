ea99cbe33649c4f6154e0cba55174687504fd165
nehal <nehalhomoda00@gmail.com>
2024-08-07 05:47:09 +0300

    update

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,98 +1,132 @@
 <template>
-    <div class="main-page">
-        <div class="header">
-            <!-- <div class="btn-wrapper"> -->
-            <button class="rules-btn">قواعد المكافات</button>
-            <!-- </div> -->
+  <div class="main-page">
+    <div class="header">
+      <button class="rules-btn">قواعد المكافات</button>
 
-            <Timer />
-        </div>
-        <div class="btn-wrapper">
-            <button @click="changeType('receiver')" :class="{ 'active': selectedType == 'receiver' }" class="">قائمه
-                الاستلام</button>
-            <button @click="changeType('senders')" :class="{ 'active': selectedType == 'senders' }">قائمه الارسال</button>
-
-        </div>
+      <Timer />
     </div>
+    <div class="btn-wrapper">
+      <button
+        @click="changeType('receiver')"
+        :class="{ active: selectedType == 'receiver' }"
+        class=""
+      >
+        قائمه الاستلام
+      </button>
+      <button
+        @click="changeType('senders')"
+        :class="{ active: selectedType == 'senders' }"
+      >
+        قائمه الارسال
+      </button>
+    </div>
+    <div class="rank-wrapper">
+      <RankingBox />
+      <RankingName />
+    </div>
+  </div>
 </template>
 
 
 <script>
-import Timer from '@/components/Timer.vue'
+import Timer from "@/components/Timer.vue";
+import RankingBox from "./RankingBox.vue";
+import RankingName from "./RankingName.vue";
 export default {
-    components: {
-        Timer
+  components: {
+    Timer,
+    RankingBox,
+    RankingName,
+  },
+  data: function () {
+    return {
+      selectedType: "",
+    };
+  },
+  methods: {
+    changeType(type) {
+      this.selectedType = type;
     },
-    data: function () {
-
-        return {
-            selectedType: ''
-        }
-    },
-    methods: {
-        changeType(type) {
-            this.selectedType = type
-
-        }
-    }
-}
-
-
+  },
+};
 </script>
 
 
 
 <style lang="scss" scoped>
+.rank-wrapper {
+  width: 90%;
+  min-height: 104vw;
+  background: url("@/assets/imgs/rank-bg.png") center no-repeat;
+  background-size: contain;
+  margin: 60px auto;
+  position: relative;
+  &::before {
+    width: 105%;
+    height: 44vw;
+    background: url("@/assets/imgs/rank-before-bg.png") center no-repeat;
+    background-size: contain;
+    position: absolute;
+    top: -10px;
+    left: -10px;
+    content: "";
+  }
+  &::after {
+    content: "";
+    width: 104%;
+    height: 34vw;
+    background: url("@/assets/imgs/rank-after-bg.png") center no-repeat;
+    background-size: contain;
+    position: absolute;
+    bottom: -10px;
+    left: -9px;
+  }
+}
 .btn-wrapper {
-    width: 100%;
-    height: 20vw;
-    display: flex;
-    justify-content: space-between;
-    margin: -60px auto;
-    align-items: center;
+  width: 100%;
+  height: 20vw;
+  display: flex;
+  justify-content: space-between;
+  margin: -60px auto;
+  align-items: center;
 
-
-
-    button {
-        width: 45%;
-        height: 100%;
-        background: url('@/assets/imgs/uncheck-btn.png') center no-repeat;
-        background-size: contain;
-        border: none;
-        outline: none;
-        padding-top: 20px;
-        color: rgb(250, 217, 150);
-        &.active{
-            background: url('@/assets/imgs/check-btn.png') center no-repeat;
-            background-size: contain;
-        }
-
+  button {
+    width: 45%;
+    height: 100%;
+    background: url("@/assets/imgs/uncheck-btn.png") center no-repeat;
+    background-size: contain;
+    border: none;
+    outline: none;
+    padding-top: 20px;
+    color: rgb(250, 217, 150);
+    &.active {
+      background: url("@/assets/imgs/check-btn.png") center no-repeat;
+      background-size: contain;
     }
-
+  }
 }
 
 .header {
-    width: 100%;
-    height: 144vw;
-    background: url('@/assets/imgs/header-bg.png') no-repeat;
-    background-size: cover;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 15px;
-    align-items: center;
-    padding-top: 370px;
+  width: 100%;
+  height: 144vw;
+  background: url("@/assets/imgs/header-bg.png") no-repeat center;
+  background-size: contain;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 370px;
 
-    .rules-btn {
-        width: 60vw;
-        height: 14vw;
-        background: url('@/assets/imgs/rules-bg.png') no-repeat;
-        background-size: contain;
-        border: none;
-        outline: none;
-        color: rgb(250, 217, 150);
-        font-weight: bold;
-    }
-
+  .rules-btn {
+    width: 50%;
+    height: 18vw;
+    background: url("@/assets/imgs/rules-bg.png") no-repeat center;
+    background-size: contain;
+    border: none;
+    outline: none;
+    color: rgb(250, 217, 150);
+    font-weight: bold;
+    font-size: 15px;
+  }
 }
 </style>
---
+++ src/components/RankingBox.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="rank-box-wrap">
+    <div class="rank-item" v-for="(item, index) in 3" :key="index">
+      <div class="top-img">
+        <img src="@/assets/imgs/avatar.jpg" alt="" />
+        <img
+          class="img-frame"
+          v-if="index == 0"
+          src="@/assets/imgs/top_frame_1.png"
+          alt=""
+        />
+        <img
+          class="item-frame"
+          v-if="index == 0"
+          src="@/assets/imgs/top1-bg.png"
+          alt=""
+        />
+        <img
+          class="img-frame"
+          v-if="index == 1"
+          src="@/assets/imgs/top_frame_2.png"
+          alt=""
+        />
+        <img
+          class="item-frame"
+          v-if="index == 1"
+          src="@/assets/imgs/top2-bg.png"
+          alt=""
+        />
+        <img
+          class="img-frame"
+          v-if="index == 2"
+          src="@/assets/imgs/top_frame_3.png"
+          alt=""
+        />
+        <img
+          class="item-frame"
+          v-if="index == 2"
+          src="@/assets/imgs/top3-bg.png"
+          alt=""
+        />
+      </div>
+      <span>refref</span>
+      <span>rferf</span>
+      <span>frerfer</span>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+</script>
+
+
+
+<style scoped lang="scss">
+.rank-box-wrap {
+  display: flex;
+  padding: 40% 5% 5% 5%;
+  gap: 5%;
+  .rank-item {
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: white;
+    .top-img {
+      width: 15vw;
+      height: 15vw;
+      position: relative;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        object-fit: cover;
+        z-index: 999;
+       
+      }
+      .img-frame {
+        width: 130%;
+        height: 146%;
+        position: absolute;
+        left: 50%;
+        top: 39%;
+        transform: translate(-50%, -50%);
+        z-index: 2;
+        
+      }
+      .item-frame{
+        width: 130%;
+        height: 146%;
+        position: absolute;
+        left: 50%;
+        top: 39%;
+        transform: translate(-50%, -50%);
+        z-index: 1;
+      }
+    }
+  }
+}
+</style>
+
+
+
+
+
+
